69c12153f8373edfdcc479412050634e60e81409
Amber <amber@cs.toronto.edu>
2014-06-13 17:37:02 -0400

    Cleaned up gross redundancies.

--- protagonist/test/test_tagger.py
+++ protagonist/test/test_tagger.py
@@ -5,17 +5,16 @@ import unittest, os
 class TaggerTest(unittest.TestCase):
 
     def setUp(self):
-        
+
         tagger.create_tagsystem({"base_directory" : "/tmp/.protagonist"})
-        self.tag_0 = "test_tag_0"
-        self.tag_1 = "test_tag_1"
-        self.tag_2 = "test_tag_2"
-        self.file_name_0 = "test_file_name_0"
-        self.file_name_1 = "test_file_name_1"
-        self.file_name_2 = "test_file_name_2"
-        open(self.file_name_0, 'w').close()
-        open(self.file_name_1, 'w').close()
-        open(self.file_name_2, 'w').close()
+
+        self.test_tags = ["test_tag_" + str(i) for i in range(2)]
+        self.file_names = ["file_name_" + str(i) for i in range(2)]
+
+        def touch(file_name):
+            open(file_name, 'w').close()
+
+        map(touch, self.file_names)
 
 if __name__ == '__main__':
     unittest.main()
