abd2a787dd099e41f833532870cb7fec668cd479
Amber <amber@cs.toronto.edu>
2014-07-03 14:45:03 -0400

    Changed the CLI name from add to tag.

--- protagonist/protag.py
+++ protagonist/protag.py
@@ -11,7 +11,7 @@ def main():
         bool_list = sys.argv[2:]
         for f in t.get_names(t.parse(bool_list)):
             sys.stdout.write(f + "\n")
-    elif sys.argv[1] == "add":
+    elif sys.argv[1] == "tag":
         file_name = sys.argv[2]
         tags = sys.argv[3:]
         for tag in tags:
